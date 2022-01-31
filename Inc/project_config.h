87a2e9859733adad3fd73f823bf0cc42c3844581
Sebastien Kister <sebastien@arianesline.com>
2022-01-31 00:26:01 -0500

    Added Ironblack color user LUT

--- Inc/project_config.h
+++ Inc/project_config.h
@@ -8,7 +8,7 @@
 // #define Y16
 
 // Values from LEP_PCOLOR_LUT_E in Middlewares/lepton_sdk/Inc/LEPTON_VID.h
-#define PSUEDOCOLOR_LUT LEP_VID_FUSION_LUT
+#define PSUEDOCOLOR_LUT LEP_VID_USER_LUT 
 
 #ifdef USART_DEBUG // this happens by 'make USART_DEBUG=1' or when USART_DEBUG is enabled above
 #undef THERMAL_DATA_UART
